0baba84aed088a39a112b54377e318ff85ce4ba9
alvon17 <73384433+alvon17@users.noreply.github.com>
2023-01-06 18:03:52 +0700

    image fixing + create pop up product not found

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Product;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class ProductController extends Controller
 {
-    public function homeProduct() {
+    public function homeProduct()
+    {
         $categories = Category::all();
 
-        return view('all.dashboard', ['categories'=>$categories]);
+        return view('all.dashboard', ['categories' => $categories]);
     }
 
-    public function detail($id) {
+    public function detail($id)
+    {
         $categories = Category::all();
         $product = Product::find($id);
         $products = Product::where([
@@ -22,29 +24,36 @@ class ProductController extends Controller
             ['id', '!=', $id]
         ])->get();
         // dd($products);
-        return view('all.detail', ['categories'=>$categories, 'product'=>$product, 'products'=>$products]);
+        return view('all.detail', ['categories' => $categories, 'product' => $product, 'products' => $products]);
     }
 
-    public function viewcategory($name) {
+    public function viewcategory($name)
+    {
         $categories = Category::all();
         $category = Category::where('name', 'like', $name)->get();
 
         $products = Product::where('category_id', 'like', $category[0]->id)->paginate(10);
+        if ($products->isEmpty()) {
+            Session::flash('message', 'Produk Belum Tersedia pada Platform');
+            return redirect()->back();
+        }
 
-        return view('all.category', ['categories'=>$categories, 'products'=>$products]);
+        return view('all.category', ['categories' => $categories, 'products' => $products]);
     }
 
-    public function adminProduct() {
+    public function adminProduct()
+    {
         $categories = Category::all();
         $products = Product::paginate(5, ['*'], 'products');
         $categories2 = Category::paginate(5, ['*'], 'categories');
 
-        return view('admin.dashboard', ['categories'=>$categories, 'products'=>$products, 'categories2'=>$categories2]);
+        return view('admin.dashboard', ['categories' => $categories, 'products' => $products, 'categories2' => $categories2]);
     }
 
-    public function addProduct() {
+    public function addProduct()
+    {
         $categories = Category::all();
 
-        return view('admin.add', ['categories'=>$categories]);
+        return view('admin.add', ['categories' => $categories]);
     }
 }
