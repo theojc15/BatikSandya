3ad6fa487bf3fbae1ef62cffadb7ee615d8b2fd6
Chrylus <czar.soecahyo@binus.ac.id>
2023-01-23 16:05:25 +0700

    form checkout

--- resources/views/user/checkout.blade.php
+++ resources/views/user/checkout.blade.php
@@ -16,46 +16,51 @@
                     <div class="card">
                         <div class="card-body">
 
-                            <div class="form-group mb-3">
-                                <label class="mb-2"> Address </label>
-                                <input name="text" type="text" class="form-control" id="address" value="{{ $user->address }}">
-                            </div>
+                            <form style="all: unset !important" method="POST" action="">
+                            @csrf
 
-                            <div class="form-group mb-3">
-                                <label for="price" class="orange form-label">Total Price</label>
-                                <div class="input-group mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">IDR</span>
-                                    </div>
-                                    <input name="text" type="text" class="readonly form-control" id="address" value="Rp {{ $total }}" readonly>
+                                <div class="form-group mb-3">
+                                    <label class="mb-2"> Address </label>
+                                    <input name="text" type="text" class="form-control" id="address" value="{{ $user->address }}">
                                 </div>
-                            </div>
 
-                            <div class="form-group mb-3">
-                                <label class="mb-2"> Payment Method </label>
-                                <input name="text" type="text" class="readonly form-control" id="payment" value="BCA - 00000000" readonly>
-                            </div>
+                                <div class="form-group mb-3">
+                                    <label for="price" class="orange form-label">Total Price</label>
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">IDR</span>
+                                        </div>
+                                        <input name="text" type="text" class="readonly form-control" id="address" value="Rp {{ $total }}" readonly>
+                                    </div>
+                                </div>
 
-                            <div class="form-group mb-3">
-                                <label class="mb-2"> Account Number </label>
-                                <input name="text" type="text" class="form-control" id="accountNumber" value="Placeholder">
-                            </div>
+                                <div class="form-group mb-3">
+                                    <label class="mb-2"> Payment Method </label>
+                                    <input name="text" type="text" class="readonly form-control" id="payment" value="BCA - 00000000" readonly>
+                                </div>
 
-                            <div class="form-group mb-3">
-                                <label class="mb-2"> Account Name </label>
-                                <input name="text" type="text" class="form-control" id="accountName" value="Placeholder">
-                            </div>
+                                <div class="form-group mb-3">
+                                    <label class="mb-2"> Account Number </label>
+                                    <input name="text" type="text" class="form-control" id="accountNumber" value="Placeholder">
+                                </div>
 
-                            <label class="mb-2"> Proof of Payment </label>
-                            <div class="input-group custom-file-button mb-3">
-                                <label class="input-group-text input-group-file" for="inputGroupFile">Choose a File</label>
-                                <input type="file" class="orange form-control" id="inputGroupFile" name="photo">
-                            </div>
+                                <div class="form-group mb-3">
+                                    <label class="mb-2"> Account Name </label>
+                                    <input name="text" type="text" class="form-control" id="accountName" value="Placeholder">
+                                </div>
+
+                                <label class="mb-2"> Proof of Payment </label>
+                                <div class="input-group custom-file-button mb-3">
+                                    <label class="input-group-text input-group-file" for="inputGroupFile">Choose a File</label>
+                                    <input type="file" class="orange form-control" id="inputGroupFile" name="photo">
+                                </div>
 
 
-                            <div class="purchase-btn d-flex justify-content-center mt-4">
-                                <a style="text-decoration: none;width: 35%" href="/purchase"><button style="width: 100%">Purchase</button></a>
-                            </div>
+                                <div class="purchase-btn d-flex justify-content-center mt-4">
+                                    <button type="submit">Purchase</button>
+                                </div>
+
+                            </form>
                         </div>
                     </div>
                 </div>
